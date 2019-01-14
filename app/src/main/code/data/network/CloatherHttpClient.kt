d3b1d96504d5abf1689e991ffd5dc32d1cbcdad8
Kirill Chirkin <geimerchannel@gmail.com>
2019-01-14 23:54:09 +0300

    WIP добавление пустого интро

--- app/src/main/code/data/network/CloatherHttpClient.kt
+++ app/src/main/code/data/network/CloatherHttpClient.kt
@@ -3,6 +3,7 @@ package data.network
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
 import java.util.concurrent.TimeUnit
 
 /**
