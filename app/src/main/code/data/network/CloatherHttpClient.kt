3b4747221d887dee4b442663c14386c03690676c
Keyrillanskiy <geimerchannel@gmail.com>
2019-01-15 08:51:46 +0300

    Fixed gradle dependencies

--- app/src/main/code/data/network/CloatherHttpClient.kt
+++ app/src/main/code/data/network/CloatherHttpClient.kt
@@ -4,6 +4,7 @@ import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import retrofit2.converter.jackson.JacksonConverterFactory
 import java.util.concurrent.TimeUnit
 
 /**
